f100fa68d61381ca65cf510f5b1ec6daaafb8ec7
i862368 <gabriel.weber@sap.com>
2017-07-27 16:31:35 -0300

    Change file name

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "gochallenges/readchangefile"
 
 func main(){
-	readchangefile.ChangeFile("/home/I862368/Downloads/Data_PoC/index/SAP_RSM/sa/sap.rsm.xs.db%3a%3aRSM.ObjectProperty/data.csv")
+	readchangefile.ChangeFile("dummyFile.csv")
 }
