28bce8c8c647cd73b2e231ed4316ae558980ebd0
i862368 <gabriel.weber@sap.com>
2017-07-27 16:29:54 -0300

    Change files

--- main.go
+++ main.go
@@ -1,1 +1,7 @@
-package gochallenges
+package main
+
+import "gochallenges/readchangefile"
+
+func main(){
+	readchangefile.ChangeFile("/home/I862368/Downloads/Data_PoC/index/SAP_RSM/sa/sap.rsm.xs.db%3a%3aRSM.ObjectProperty/data.csv")
+}
--- readchangefile/ReadChangeFile.go
+++ readchangefile/ReadChangeFile.go
@@ -1,1 +1,36 @@
 package readchangefile
+
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func ChangeFile(file string){
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fileStr := string(data)
+
+	fileSpl := strings.Split(fileStr, "\n")
+
+	for i := range fileSpl {
+
+		lineSpl := strings.Split(fileSpl[i], ",")
+		if len(lineSpl) < 4 {
+			continue
+		}
+		lineSpl[4] = "0"
+		lineSpl[8] = "0"
+		fileSpl[i] = strings.Join(lineSpl, ",")
+	}
+
+	fileStr = strings.Join(fileSpl, "\n")
+
+	ioutil.WriteFile(file, []byte(fileStr), 0644)
+}
