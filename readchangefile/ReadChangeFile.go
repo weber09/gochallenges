0a21a068b59d54299bbd4182eb5865c32b04e589
weber09 <gabrieldsweber@gmail.com>
2017-07-27 16:45:11 -0300

    Insert status messages on each step

--- readchangefile/ReadChangeFile.go
+++ readchangefile/ReadChangeFile.go
@@ -4,9 +4,13 @@ import (
 	"io/ioutil"
 	"log"
 	"strings"
+	"fmt"
 )
 
 func ChangeFile(file string){
+
+	fmt.Printf("Reading file %d\n", file)
+
 	data, err := ioutil.ReadFile(file)
 	if err != nil {
 		log.Fatal(err)
@@ -19,6 +23,8 @@ func ChangeFile(file string){
 
 	fileSpl := strings.Split(fileStr, "\n")
 
+	fmt.Printf("Changing file")
+
 	for i := range fileSpl {
 
 		lineSpl := strings.Split(fileSpl[i], ",")
@@ -26,6 +32,7 @@ func ChangeFile(file string){
 			continue
 		}
 		lineSpl[4] = "0"
+		lineSpl[5] = "0"
 		lineSpl[8] = "0"
 		fileSpl[i] = strings.Join(lineSpl, ",")
 	}
@@ -33,4 +40,6 @@ func ChangeFile(file string){
 	fileStr = strings.Join(fileSpl, "\n")
 
 	ioutil.WriteFile(file, []byte(fileStr), 0644)
+
+	fmt.Printf("File changed and saved succesfully!")
 }
